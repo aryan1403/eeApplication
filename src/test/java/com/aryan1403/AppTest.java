1c84860f1d9be13b5cb5d91e6e7385ff53fb58ec
Aaryan <aaryan14032006@gmail.com>
2021-09-14 10:59:23 +0530

    Test File Updated

--- src/test/java/com/aryan1403/AppTest.java
+++ src/test/java/com/aryan1403/AppTest.java
@@ -1,12 +1,8 @@
 package com.aryan1403;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
     /**
      * Rigorous Test.
