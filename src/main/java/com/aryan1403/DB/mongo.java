90790142540a0417c46b697c1b65465b36cf3778
Aaryan <aaryan14032006@gmail.com>
2021-09-14 10:54:05 +0530

    Comments Added

--- src/main/java/com/aryan1403/DB/mongo.java
+++ src/main/java/com/aryan1403/DB/mongo.java
@@ -5,15 +5,16 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
 import org.bson.Document;
 
 public class mongo {
+    // Mongo Settings
     MongoClient client = new MongoClient("localhost", 27017);
     MongoDatabase database = client.getDatabase("eeapplication");
     MongoCollection<Document> collection = database.getCollection("Data");
 
     public void add(supplier s) {
+        // Add the Supplier object props to the document as key value pair
         Document doc = new Document("id", s.getProductID())
             .append("pname", s.getProductName())
             .append("pid", s.getProductID())
@@ -22,9 +23,11 @@ public class mongo {
             .append("price", s.getPrice())
             .append("rating", s.getRating());
 
+        // Insert the document to the Collection
         collection.insertOne(doc);
     }
 
+    // Retreive all the Documents from the Collection 
     public FindIterable<Document> retreive() {
         return collection.find();
     }
