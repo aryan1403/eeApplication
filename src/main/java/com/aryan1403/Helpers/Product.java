e90115a98463af26416f1ea3583ace89d9fdb47b
Aaryan <aaryan14032006@gmail.com>
2021-09-14 10:54:20 +0530

    Comments Added

--- src/main/java/com/aryan1403/Helpers/Product.java
+++ src/main/java/com/aryan1403/Helpers/Product.java
@@ -3,6 +3,7 @@ package com.aryan1403.Helpers;
 import com.aryan1403.DB.mongo;
 import com.aryan1403.Merchandise.supplier;
 
+// Add the Supplier to the Database
 public class Product {
     public void add(supplier s) {
         new mongo().add(s);
